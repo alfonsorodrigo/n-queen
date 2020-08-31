26e4fdd1dad3d8839732a121ac1bf088e10bf314
Alfonso Feria <alfonsorodrigo12@gmail.com>
2020-08-30 22:25:21 -0500

    Added Postgresql

--- cuenca_app/test_n_queen_model.py
+++ cuenca_app/test_n_queen_model.py
@@ -1,14 +1,33 @@
+import os
+import time
 import unittest
 from unittest import TestCase
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import OperationalError
 from n_queen_model import NQueen, Base
 from n_queen import SolveNQueen
 
 
+DB_HOST = os.environ.get("DB_HOST")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+
+
 class ORMCreateSchemaTestCase(TestCase):
     def setUp(self) -> None:
-        self.engine = create_engine("sqlite:///:memory:")
+        while True:
+            try:
+                self.engine = create_engine(
+                    f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+                )
+                self.engine.execute("select 1")
+            except OperationalError:
+                print("Waiting for database...")
+                time.sleep(1)
+            else:
+                break
         Session = sessionmaker(bind=self.engine)
         self.session = Session()
 
@@ -19,7 +38,17 @@ class ORMCreateSchemaTestCase(TestCase):
 
 class ORMCreateExampleDataTestCase(TestCase):
     def setUp(self) -> None:
-        self.engine = create_engine("sqlite:///:memory:")
+        while True:
+            try:
+                self.engine = create_engine(
+                    f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+                )
+                self.engine.execute("select 1")
+            except OperationalError:
+                print("Waiting for database...")
+                time.sleep(1)
+            else:
+                break
         Base.metadata.create_all(self.engine)
         Session = sessionmaker(bind=self.engine)
         self.session = Session()
